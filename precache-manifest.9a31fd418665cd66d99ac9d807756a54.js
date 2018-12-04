d7d28f4550b0d2c71cce304a5b3ab80240b5144d
Hodga <hojajepbar@gmail.com>
2018-12-04 20:00:34 +0200

    Updates

---
+++ precache-manifest.9a31fd418665cd66d99ac9d807756a54.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a695b30138abc35248a",
+    "url": "/todo/static/js/runtime~main.8a695b30.js"
+  },
+  {
+    "revision": "05bc21939a86e2d82702",
+    "url": "/todo/static/js/main.05bc2193.chunk.js"
+  },
+  {
+    "revision": "aac75bbadb65a070ad02",
+    "url": "/todo/static/js/1.aac75bba.chunk.js"
+  },
+  {
+    "revision": "05bc21939a86e2d82702",
+    "url": "/todo/static/css/main.dd2f3751.chunk.css"
+  },
+  {
+    "revision": "078fa1370dae2da684348b78cd85a9fb",
+    "url": "/todo/index.html"
+  }
+];
